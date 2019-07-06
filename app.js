02eae3ec62a8e9dbd4ed22fe796eaac31ea34073
Amitesh <17ucs018@lnmiit.ac.in>
2019-07-06 20:57:10 +0530

    Comment Functionality Working

--- app.js
+++ app.js
@@ -9,6 +9,7 @@ var Comment = require("./models/comment");
 mongoose.connect("mongodb://localhost/yelp_camp", {useNewUrlParser: true});
 app.use(bodyParser.urlencoded({extended: true}));
 app.set("view engine", "ejs");
+app.use(express.static(__dirname + "/public"));
 
 
 seedDB();
